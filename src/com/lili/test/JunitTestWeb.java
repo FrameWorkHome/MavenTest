ad956de5c8c1519ff1a65c518d92101ad5e0ff53
lili <943070992@qq.com>
2017-08-27 21:28:29 +0800

    junit测试代码1231

---
+++ src/com/lili/test/JunitTestWeb.java
@@ -0,0 +1,14 @@
+package com.lili.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JunitTestWeb {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
